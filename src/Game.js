8a6a1e13c9ed1971d01399f64ac1d96500992e36
LinhLPV <lephamvanlinh@gmail.com>
2018-12-14 00:48:04 +0700

    create board game

---
+++ src/Game.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+
+function Square(props) {
+    return(
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+
+}
+class Game extends React.Component
+{
+    render()
+    {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-infor">
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Board extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.squares ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+    renderSquare(i){
+        return(
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+    render(){
+        const status = 'Next player: ' + this.state.xIsNext ? 'X' : 'O';
+        return(
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+export default Game
